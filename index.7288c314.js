71e6b1cc707465665317f977d43acd03bd0d221e
olshum <olshum8@gmail.com>
2024-09-08 19:08:41 +0300

    make build

---
+++ index.7288c314.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientHeight,l=t.clientHeight,i=e.clientWidth,n=t.clientWidth;t.style.top="".concat((c-l)/2,"px"),t.style.left="".concat((i-n)/2,"px");
+//# sourceMappingURL=index.7288c314.js.map
